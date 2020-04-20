26947ee0f0ad433fc82958b8947337ee9627284f
william figueredo <william_figueredo@hotmail.com>
2020-04-20 00:47:43 -0300

    ajuste

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ export default function App() {
               style={styles.likeText}              
               testID={`repository-likes-${repository.id}`}
             >
-              {repository.likes} curtida {repository.likes > 1 ? 's': ''}
+              {repository.likes} curtida{repository.likes > 1 ? 's': ''}
             </Text>
           </View>
 
